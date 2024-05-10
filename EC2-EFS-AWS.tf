c51f05b88ea2d1c348391006f44f5b27b383635f
Lucas Gregorio Bueno <lucas.bueno@info.local>
2024-05-10 16:18:01 -0300

    <Update>[GithubActions]: Alterando a para o terraform criar 2 instancias separadas ao invez de 2 ao mesmo tempo

--- EC2-EFS-AWS.tf
+++ EC2-EFS-AWS.tf
@@ -25,8 +25,7 @@ variable "custom_data_script" {
 }
 
 # Criar EC2 Linux
-resource "aws_instance" "linux_EFS" {
-  count                       = 2
+resource "aws_instance" "linux_EFS_1" {
   ami                         = "ami-04ff98ccbfa41c9ad" # Amazon linux 2
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-08fedf52cca8dde8f"
@@ -35,13 +34,32 @@ resource "aws_instance" "linux_EFS" {
   vpc_security_group_ids      = ["sg-0c1f6f466d3838230"] #["${aws_security_group.instance_sg.id}"]
   user_data                   = var.custom_data_script
   tags = {
-    Name = "Linux-EFS-${count.index}"
+    Name = "Linux-EFS-1"
+  }
+}
+
+resource "aws_instance" "linux_EFS_2" {
+  ami                         = "ami-04ff98ccbfa41c9ad" # Amazon linux 2
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-08fedf52cca8dde8f"
+  key_name                    = "vockey" # Não esqueca de gerar a chave  pública e privada para este nome!
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["sg-0c1f6f466d3838230"] #["${aws_security_group.instance_sg.id}"]
+  user_data                   = var.custom_data_script
+  tags = {
+    Name = "Linux-EFS-2"
   }
 }
 
 # Exibir IP público das maquinas
-output "instance_ips" {
-  description = "IP Publico da Instancia EC2 linux"
-  value       = aws_instance.linux_EFS[*].public_ip
+output "instance_ip_1" {
+  description = "IP Publico da 1 Instancia EC2 linux"
+  value       = aws_instance.linux_EFS_1.public_ip
+}
+
+# Exibir IP público das maquinas
+output "instance_ip_2" {
+  description = "IP Publico da 2 Instancia EC2 linux"
+  value       = aws_instance.linux_EFS_2.public_ip
 }
 
